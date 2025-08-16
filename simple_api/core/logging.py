e2ee17b27e9dd0df12d635fa56a58289b8cdef6e
RobertoFORTs <bobfortesneto@gmail.com>
2025-08-15 21:47:39 -0400

    enhancement: improve logging

--- simple_api/core/logging.py
+++ simple_api/core/logging.py
@@ -1,12 +1,25 @@
+from simple_api.core.config import settings
 import structlog
+import sys
+
+ENV = settings.env  # "dev" or "prod"
+
+if ENV == "dev":
+    renderer = structlog.dev.ConsoleRenderer(colors=True)
+else:
+    renderer = structlog.processors.JSONRenderer()
 
 structlog.configure(
     processors=[
         structlog.processors.add_log_level,
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.dict_tracebacks,
-        structlog.processors.JSONRenderer(),
-    ]
+        renderer,
+    ],
+    wrapper_class=structlog.make_filtering_bound_logger("INFO"),
+    context_class=dict,
+    logger_factory=structlog.PrintLoggerFactory(file=sys.stdout),
+    cache_logger_on_first_use=True,
 )
 
 log = structlog.get_logger()
